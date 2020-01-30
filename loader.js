481850faa07ea97f5ca97bac836ce769d9f35427
Mukesh Bari <59745050+TheNaughtyGuy@users.noreply.github.com>
2020-01-30 19:29:05 +0530

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
+
+
+
+.no-js #loader-wrapper {
+    display: none;
+}
+.no-js h1 {
+    color: #222222;
+}
